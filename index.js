ed2e08ab697631400564e8a2773652e8d6be36a9
Eduard Urbach <e.urbach@gmail.com>
2015-11-21 12:45:47 +0900

    Split functions into multiple files
    Also fixed double ajaxify call

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 module.exports = {
 	scripts: [
-		'lib/kaze'
+		'lib/kaze',
+		'lib/functions/ajaxifyLinks',
+		'lib/functions/emit',
+		'lib/functions/executeScripts',
+		'lib/functions/fadeIn',
+		'lib/functions/fadeOut',
+		'lib/functions/get',
+		'lib/functions/getJSON',
+		'lib/functions/getTransitionEventName',
+		'lib/functions/loadURL',
+		'lib/functions/markActiveLinks',
+		'lib/functions/onResponse'
 	]
 }
